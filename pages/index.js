9039ccdd50bd09d742aa2372abf78964bbf9cb96
Paulo Henrique <89206889+PauloHPB97@users.noreply.github.com>
2022-03-21 18:07:30 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,175 @@
+import {Box, Button, Text, TextField, Image} from '@skynexui/components';
+import React from 'react';
+import { useRouter } from 'next/router';
+import appConfig from '../config.json'
+
+function Titulo(props) {
+    const Tag = props.tag || 'h1';
+    return (
+        <>
+            <Tag>{props.children}</Tag>
+            <style jsx>{`
+                ${Tag} {
+                    color: ${appConfig.theme.colors.neutrals["200"]};
+                    font-size: 24px;
+                    font-weight: 600;
+                }
+            `}</style>
+        </>
+    )
+}
+
+// Componente React
+// function HomePage() {
+//     // JSX
+//     return (
+//     <div>
+//         <GlobalStyle />
+//         <Titulo tag="h2">Boas vindas de volta!</Titulo>
+//         <h2>Discord - Alura Matrix</h2>
+        
+//     </div>
+//     )
+// }
+//export default HomePage
+
+export default function PaginaInicial() {
+    // const username = 'omariosouto';
+    const [username, setUsername] = React.useState('paulohpb97');
+    const roteamento = useRouter();
+
+    return (
+      <>
+        <Box
+          styleSheet={{
+            display: 'flex', alignItems: 'center', justifyContent: 'center',
+            backgroundImage: 'url(https://images.pexels.com/photos/240572/pexels-photo-240572.jpeg?cs=srgb&dl=pexels-joonas-k%C3%A4%C3%A4ri%C3%A4inen-240572.jpg&fm=jpg)',
+            backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+          }}
+        >
+          <Box
+            styleSheet={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              flexDirection: {
+                xs: 'column',
+                sm: 'row',
+              },
+              width: '100%', maxWidth: '700px',
+              borderRadius: '5px', padding: '32px', margin: '16px',
+              boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+              backgroundColor: "rgba(40,40,40,0.92)",
+            }}
+          >
+            {/* Formulário */}
+            <Box
+              as="form"
+              onSubmit={function(infosDoEvento) {
+                infosDoEvento.preventDefault();
+                console.log('Alguém submeteu o form!');
+                roteamento.push(`/chat?username=${username}`)
+
+                //window.location.href = '/chat';
+              }}
+              styleSheet={{
+                display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+                width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+              }}
+            >
+              <Titulo tag="h2">Boas vindas de volta!</Titulo>
+              <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[200] }}>
+                {appConfig.name}
+              </Text>
+  
+              {/* <input 
+                type="text"
+                value={username}
+                onChange={function(event) {
+                  console.log("O usuário digitou!", event.target.value);
+                  // Onde está o valor?
+                  const valor = event.target.value;
+                  // Trocar o valor da variável através do React e avise quem precisa
+                  setUsername(valor);
+                }}
+              /> */}
+
+              <TextField
+                value={username}
+                onChange={function(event) {
+                  console.log("O usuário digitou!", event.target.value);
+
+                  // Onde está o valor?
+                  const valor = event.target.value;                  
+                  // Trocar o valor da variável através do React e avise quem precisa
+                  setUsername(valor);
+
+                }}
+                fullWidth
+                textFieldColors={{
+                  neutral: {
+                    textColor: appConfig.theme.colors.neutrals[200],
+                    mainColor: appConfig.theme.colors.neutrals[900],
+                    mainColorHighlight: "#00BBF9",
+                    backgroundColor: appConfig.theme.colors.neutrals[600],
+                  },
+                }}
+              />
+              <Button
+                type='submit'
+                label='Entrar'
+                fullWidth
+                buttonColors={{
+                  contrastColor: appConfig.theme.colors.neutrals["000"],
+                  mainColor: "#00BBF9",
+                  mainColorLight: appConfig.theme.colors.primary[400],
+                  mainColorStrong: "#008fbe",
+                }}
+                
+              />
+            </Box>
+            {/* Formulário */}
+  
+  
+            {/* Photo Area */}
+            <Box
+              styleSheet={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                maxWidth: '200px',
+                padding: '16px',
+                backgroundColor: appConfig.theme.colors.neutrals[700],
+                border: '1px solid',
+                borderColor: appConfig.theme.colors.neutrals[999],
+                borderRadius: '10px',
+                flex: 1,
+                minHeight: '240px',
+              }}
+            >
+              <Image
+                styleSheet={{
+                  borderRadius: '50%',
+                  marginBottom: '16px',
+                }}
+                src={`https://github.com/${username}.png`}
+                
+              />
+              <Text
+                variant="body4"
+                styleSheet={{
+                  color: appConfig.theme.colors.neutrals[200],
+                  backgroundColor: appConfig.theme.colors.neutrals[900],
+                  padding: '3px 10px',
+                  borderRadius: '1000px'
+                }}
+              >
+                {username}
+              </Text>
+            </Box>
+            {/* Photo Area */}
+          </Box>
+        </Box>
+      </>
+    );
+  }
